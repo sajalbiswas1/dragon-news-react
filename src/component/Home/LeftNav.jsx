61e8b506478df97a686fcbc9889742680827c08f
Programing Hero <sajalb1899@gmail.com>
2023-10-04 08:32:15 +0600

    leftNav categoris & rightNav google login & social link added

--- src/component/Home/LeftNav.jsx
+++ src/component/Home/LeftNav.jsx
@@ -14,7 +14,7 @@ const LeftNav = () => {
             <h3 className="text-xl font-semibold mb-5">All Category</h3>
             <div className="">
                 {
-                    categories.map(categori => <Link key={categori.id}><button className="focus:text-[#403F3F] text-left focus:font-semibold pl-12 py-4 rounded-md focus:bg-[#E7E7E7] w-full text-[#9F9F9F] " >{categori.name}</button></Link>)
+                    categories.map(categori => <Link key={categori.id}><button className="focus:text-[#403F3F] text-left focus:font-semibold pl-12 py-4 rounded-md focus:bg-[#E7E7E7] hover:bg-[#E7E7E7] w-full text-[#9F9F9F] " >{categori.name}</button></Link>)
                 }
             </div>
         </div>
--- src/component/Home/RightNav.jsx
+++ src/component/Home/RightNav.jsx
@@ -1,17 +1,33 @@
-import { AiOutlineGithub, AiOutlineGoogle } from 'react-icons/ai';
+import { AiOutlineGithub, AiOutlineGoogle, AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { GrFacebookOption } from 'react-icons/gr';
 
 const RightNav = () => {
     return (
-        <div className="col-span-1 border">
-            <h3 className="text-xl font-semibold mb-5">With LogIn</h3>
-            <div className='flex items-center hover:text-blue-700  mx-2 py-2 justify-center gap-3 hover:border-blue-700 rounded-md border-2 mb-4'>
-            <AiOutlineGoogle></AiOutlineGoogle>
-            <p>LogIn with google</p>
-            
+        <div>
+            <div className="col-span-1 border">
+                <h3 className="text-xl font-semibold mb-5">With LogIn</h3>
+                <div className='flex items-center hover:text-blue-700  mx-2 py-2 justify-center gap-3 hover:border-blue-700 rounded-md border-2 mb-4 border-black'>
+                    <AiOutlineGoogle></AiOutlineGoogle>
+                    <p>LogIn with google</p>
+
+                </div>
+                <div className='flex items-center hover:text-blue-700  mx-2 py-2 justify-center gap-3 hover:border-blue-700 rounded-md border-2 border-black'>
+                    <AiOutlineGithub></AiOutlineGithub>
+                    <p>LogIn with google</p>
+                </div>
             </div>
-            <div className='flex items-center hover:text-blue-700  mx-2 py-2 justify-center gap-3 hover:border-blue-700 rounded-md border-2'>
-            <AiOutlineGithub></AiOutlineGithub>
-            <p>LogIn with google</p>
+            <h1 className='text-xl font-semibold text-[#403F3F] mb-5 mt-9'>Find Us Zone</h1>
+            <div className='flex border rounded-t-lg gap-2 p-4 items-center'>
+                <p className='bg-slate-100 rounded-full p-2'><GrFacebookOption className='text-blue-800 w-5 h-5'></GrFacebookOption></p>
+                <p>FaceBook</p>
+            </div>
+            <div className='flex border-x gap-2 p-4 items-center'>
+                <p className='bg-slate-100 rounded-full p-2'><AiOutlineTwitter className='text-blue-400 w-5 h-5'></AiOutlineTwitter></p>
+                <p>Twitter</p>
+            </div>
+            <div className='flex border rounded-b-lg gap-2 p-4 items-center'>
+                <p className='bg-slate-100 rounded-full p-2'><AiOutlineInstagram className='text-rose-500 w-5 h-5'></AiOutlineInstagram></p>
+                <p>Instagram</p>
             </div>
         </div>
     );
